77894109619c6f2dfd3c6ed855311a83602683f2
luare123 <luarbotelhoteste@gmail.com>
2023-03-14 15:55:26 -0300

    ultima att 14/03/2023

--- Controller/EnderecoController.php
+++ Controller/EnderecoController.php
@@ -2,6 +2,7 @@
 
 namespace ApiCep\Controller;
 
+use ApiCep1\Model\EnderecoModel as ModelEnderecoModel;
 use App\Model\{ EnderecoModel, CidadeModel };
 use Exception;
 
@@ -25,5 +26,22 @@ class EnderecoController extends Controller
 
     public static function getCidadesByUF() : void
     {
+    }
+
+    public static function getCepByLogradouro() : void
+    {
+        try
+        {
+            $logradouro = $_GET['logradouro'];
+
+            $model = new EnderecoModel();
+            $model->getCepByLogradouro($logradouro);
+
+            parent::getResponseAsJSON($model->rows);
+        }
+        catch (Exception $e)
+        {
+            parent::getResponseAsJSON($e);
+        }
     }
 }
---
+++ Model/EnderecoModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ApiCep1\Model;
+
+use ApiCep\DAO\EnderecoDAO as DAOEnderecoDAO;
+use App\DAO\EnderecoDAO;
+use Exception;
+
+class EnderecoModel extends Model
+{
+    public $id_logradouro , $tipo, $descricao , $id_cidade , $uf , $complemento, $descricao_sem_numero, $descricao_cidade, $codigo_cidade_ibge , $descricao_bairro;
+
+    public $arr_cidades;
+
+    public function getLogradouroByBairroAndCidade(string $bairro, int $id_cidade)
+    {
+        try 
+        {
+            $dao = new DAOEnderecoDAO();
+
+            $this->rows = $dao->selectLogradouroByBairroAndCidade($bairro, $id_cidade);
+
+        }
+        catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function getLogradouraByCep(int $cep)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
+        }
+        catch(Exception $e)
+        {
+            throw $e;
+        }
+    }
+
+    public function getCepByLogradouro ($logradouro)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
+
+            $this->rows = $dao->selectCepByLogradouro($logradouro);
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+
+}
